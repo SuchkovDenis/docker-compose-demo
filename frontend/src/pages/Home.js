26d10799218aa5f03fe1546ed48d7257120e3986
Suchkov Denis <denis@suchkov.tech>
2024-11-24 20:43:26 +0000

    init naive

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import ProductList from "../components/ProductList";
+import { TextField, Container } from "@mui/material";
+import API_BASE_URL from "../config";
+
+function Home() {
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Если строка поиска пустая, загружаем все продукты
+    if (!searchTerm) {
+      fetch(`${API_BASE_URL}/api/products/`)
+        .then((response) => response.json())
+        .then((data) => setProducts(data))
+        .catch((error) => console.error("Error fetching products:", error));
+    } else {
+      // Иначе ищем продукты по поисковому запросу
+      fetch(`${API_BASE_URL}/api/products/search?query=${searchTerm}`)
+        .then((response) => response.json())
+        .then((data) => setProducts(data))
+        .catch((error) => console.error("Error searching products:", error));
+    }
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <Container>
+      <TextField
+        label="Search products"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        onChange={handleSearchChange}
+      />
+      <ProductList products={products} />
+    </Container>
+  );
+}
+
+export default Home;
